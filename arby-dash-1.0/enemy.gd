fe56d1802e561515f8fba3caf6fc695753a1fb41
jampat10-ops <jampat10@kingshigh.school.nz>
2026-06-10 09:49:23 +1200

    v7

--- arby-dash-1.0/enemy.gd
+++ arby-dash-1.0/enemy.gd
@@ -1,8 +1,25 @@
 extends CharacterBody2D
-@onready var target=$"../player"
-var speed=150
+
+@onready var target = $"../player"
+var speed = 150
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 func _physics_process(delta: float) -> void:
-	var direction=(target.position-position).normalized()
-	velocity=direction * speed
+	# Add gravity so it stays on ground
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	# Chase player horizontally
+	var direction = (target.position - position).normalized()
+	velocity.x = direction.x * speed
+	
+	# Jump if player is above OR if hitting a wall
+	if is_on_floor() and (target.position.y < position.y - 10 or is_on_wall()):
+		velocity.y = -400
+	
 	look_at(target.position)
 	move_and_slide()
+	
+	# Kill player on touch
+	if is_instance_valid(target) and position.distance_to(target.position) < 72:
+		target.queue_free()
