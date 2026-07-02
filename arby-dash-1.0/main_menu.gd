f3d07891a15fd1af2a20e1e338f04fc2ad9c3d86
jampat10-ops <jampat10@kingshigh.school.nz>
2026-07-02 14:20:04 +1200

    v12 Error

--- arby-dash-1.0/main_menu.gd
+++ arby-dash-1.0/main_menu.gd
@@ -2,12 +2,12 @@ extends Node
 
 func _on_easy_pressed():
 	Global.speed = 100
-	get_tree().change_scene_to_file("res://Level1.tscn")
+	get_tree().change_scene_to_file("res://stuff/Level 1.tscn")
 
 func _on_medium_pressed():
 	Global.speed = 122
-	get_tree().change_scene_to_file("res://Level1.tscn")
+	get_tree().change_scene_to_file("res://stuff/Level 1.tscn")
 
 func _on_hard_pressed():
 	Global.speed = 145
-	get_tree().change_scene_to_file("res://Level1.tscn")
+	get_tree().change_scene_to_file("res://stuff/Level 1.tscn")
