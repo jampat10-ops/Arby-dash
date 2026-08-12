07ab539fa86873e74b54e10ba12797067830cbd9
jampat10-ops <jampat10@kingshigh.school.nz>
2026-08-13 09:48:23 +1200

    v22

--- arby-dash-1.0/enemy.gd
+++ arby-dash-1.0/enemy.gd
@@ -11,3 +11,4 @@ func _physics_process(delta: float) -> void:
 	# Kill player on touch
 	if is_instance_valid(target) and position.distance_to(target.position) < 70:
 		target.queue_free()
+		get_tree().change_scene_to_file("res://lose_screen.tscn")
